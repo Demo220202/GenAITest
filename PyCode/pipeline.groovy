b92015849f262f650e37bd263782902778655c7c
z-aditya-pradhan <163983105+z-aditya-pradhan@users.noreply.github.com>
2025-03-24 01:09:56 +0530

    Update pipeline.groovy

--- PyCode/pipeline.groovy
+++ PyCode/pipeline.groovy
@@ -10,6 +10,7 @@ pipeline{
         stage ('Resource Group and OpenAI Resource Creation'){
             steps{
                 dir("PyCode"){
+                    sh "sudo su"
                     sh "python3 -m venv venv"
                     sh "source venv/bin/activate"
                     sh "pip install -r requirements.txt"
