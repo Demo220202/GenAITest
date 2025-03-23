f5bec9fdefab31afc111af9e8366066198b3fdb6
z-aditya-pradhan <163983105+z-aditya-pradhan@users.noreply.github.com>
2025-03-24 01:29:46 +0530

    Update pipeline.groovy

--- PyCode/pipeline.groovy
+++ PyCode/pipeline.groovy
@@ -10,7 +10,7 @@ pipeline{
         stage ('Resource Group and OpenAI Resource Creation'){
             steps{
                 dir("PyCode"){
-                    sh """
+                    sh '''
                         export PATH="$HOME/.pyenv/bin:$PATH"
                         eval "$(pyenv init --path)"
                         pyenv install -s 3.8.10
@@ -21,7 +21,7 @@ pipeline{
                         pip install --upgrade pip
                         pip install -r requirements.txt
                         python3 GenAI_automation.py --subscription_id $subscription_id --region $rg_region --deployment_model_name $dep_model_name --deployment_model_version $dep_model_version --brands $brand_names
-                    """
+                    '''
                 }
             }
         }
