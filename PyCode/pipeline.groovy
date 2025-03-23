e30b45117408e09a0079ce6be565f8fc49b25012
z-aditya-pradhan <163983105+z-aditya-pradhan@users.noreply.github.com>
2025-03-24 01:01:26 +0530

    Update pipeline.groovy

--- PyCode/pipeline.groovy
+++ PyCode/pipeline.groovy
@@ -13,7 +13,7 @@ pipeline{
                     sh "python3 -m venv venv"
                     sh "source venv/bin/activate"
                     sh "pip install -r requirements.txt"
-                    sh "python3 GenAI_automation.py --subscription_id $subscription_id --region $rg_region --deployment_model_name $dep_model_name --deployment_model_version $dep_model_version --brands $brand_names --client_id ARM_CLIENT_ID --client_secret ARM_CLIENT_SECRET --tenant_id ARM_TENANT_ID"
+                    sh "python3 GenAI_automation.py --subscription_id $subscription_id --region $rg_region --deployment_model_name $dep_model_name --deployment_model_version $dep_model_version --brands $brand_names"
                 }
             }
         }
@@ -47,7 +47,7 @@ pipeline{
         failure {
             sh "pwd"
             dir('PyCode'){
-                sh "python3 GenAI_automation_Revert.py --subscription_id $subscription_id --client_id ARM_CLIENT_ID --client_secret ARM_CLIENT_SECRET --tenant_id ARM_TENANT_ID"
+                sh "python3 GenAI_automation_Revert.py --subscription_id $subscription_id"
                 //sh "python3 GenAI_automation_P2_Revert.py <command_args>"
                 sh "rm output_json.json"
                 sh "deactivate"
