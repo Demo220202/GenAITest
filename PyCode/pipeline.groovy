174e16928b94e7f33ee94f43143d9d69b8b241e0
z-aditya-pradhan <163983105+z-aditya-pradhan@users.noreply.github.com>
2025-04-02 17:41:15 +0530

    Update pipeline.groovy

--- PyCode/pipeline.groovy
+++ PyCode/pipeline.groovy
@@ -7,43 +7,7 @@ pipeline{
     }
     stages{
         
-        // stage ('Resource Group and OpenAI Resource Creation'){
-        //     steps{
-        //         dir("PyCode"){
-        //             sh '''
-        //                 python3.10 -m venv venv 
-        //                 . venv/bin/activate
-        //                 pip install --upgrade pip
-        //                 pip install -r requirements.txt
-        //                 python3 GenAI_automation.py --subscription_id $subscription_id --region $rg_region --deployment_model_name $dep_model_name --deployment_model_version $dep_model_version --brands $brand_names
-        //             '''
-        //         }
-        //     }
-        // }
-
-        // stage ('Deployment Resource Creation and Enabling Dynamic Quota'){
-        //     steps{
-        //         dir("PyCode"){
-        //             sh '''
-        //                 . venv/bin/activate
-        //                 python3 GenAI_automation_P2.py
-        //             '''
-        //         }
-        //     }
-        // }
-
-        // stage ('Creation of Action Group and Alerts'){
-        //     steps{
-        //         dir("PyCode"){
-        //             sh '''
-        //                 . venv/bin/activate
-        //                 python3 ActionGroupNAlerts.py
-        //             '''
-        //         }
-        //     }
-        // }
-
-        stage ('Checking DB Conection!'){
+        stage ('Resource Group and OpenAI Resource Creation'){
             steps{
                 dir("PyCode"){
                     sh '''
@@ -51,37 +15,86 @@ pipeline{
                         . venv/bin/activate
                         pip install --upgrade pip
                         pip install -r requirements.txt
+                        python3 GenAI_automation.py --subscription_id $subscription_id --region $rg_region --deployment_model_name $dep_model_name --deployment_model_version $dep_model_version --brands $brand_names
+                    '''
+                }
+            }
+        }
+
+        stage ('Deployment Resource Creation and Enabling Dynamic Quota'){
+            steps{
+                dir("PyCode"){
+                    sh '''
+                        . venv/bin/activate
+                        python3 GenAI_automation_P2.py
+                    '''
+                }
+            }
+        }
+
+        stage ('Creation of Action Group and Alerts'){
+            steps{
+                dir("PyCode"){
+                    sh '''
+                        . venv/bin/activate
+                        python3 ActionGroupNAlerts.py
+                    '''
+                }
+            }
+        }
+
+        stage ('Checking DB Conection!'){
+            steps{
+                dir("PyCode"){
+                    sh '''
+                        //python3.10 -m venv venv 
+                        . venv/bin/activate
+                        //pip install --upgrade pip
+                        //pip install -r requirements.txt
                         python3 ProdDBConnCheck.py
+                    '''
+                }
+            }
+        }
+
+        stage ('DB Insertions'){
+            
+            steps{
+                dir("PyCode"){
+                    sh ''' 
+                        . venv/bin/activate
+                        //python3 AzureGenAIResourceDBInsertions.py --env $env_main  --user_email $user_email
+                        //python3 ProdCoachDBInsertion.py --env $env_pa  --user_email $user_email
                     '''
                 }
             }
         }
         
     }
-    //post {
-        //success {
-            //dir('PyCode') {
-                //sh """
-                //    cat openai_resources.json
-                //    rm openai_resources.json
-                //    cat alert_resources.json
-                //    rm alert_resources.json
-                //    rm -rf venv
-            //    """
-            //}
-        //}
-        //failure {
-            //dir('PyCode'){
-            //    sh '''
-            //        . venv/bin/activate
-            //        python3 GenAI_automation_Revert.py --subscription_id $subscription_id
-            //        python3 GenAI_automation_P2_Revert.py
-            //        python3 ActionGroupNAlerts_Revert.py
-            //        rm openai_resources.json
-            //        rm alert_resources.json
-            //        rm -rf venv
-            //    '''
-            //}
-        //}
-    //}
+    post {
+        success {
+            dir('PyCode') {
+                sh """
+                   cat openai_resources.json
+                   rm openai_resources.json
+                   cat alert_resources.json
+                   rm alert_resources.json
+                   rm -rf venv
+               """
+            }
+        }
+        failure {
+            dir('PyCode'){
+               sh '''
+                   . venv/bin/activate
+                   python3 GenAI_automation_Revert.py --subscription_id $subscription_id
+                   python3 GenAI_automation_P2_Revert.py
+                   python3 ActionGroupNAlerts_Revert.py
+                   rm openai_resources.json
+                   rm alert_resources.json
+                   rm -rf venv
+               '''
+            }
+        }
+    }
 }
