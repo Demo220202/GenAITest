89d3991426352d78422de8333f4c70876a89e29b
z-aditya-pradhan <163983105+z-aditya-pradhan@users.noreply.github.com>
2025-04-03 11:47:27 +0530

    Update ProdCoachDBInsertions.py

--- PyCode/ProdCoachDBInsertions.py
+++ PyCode/ProdCoachDBInsertions.py
@@ -36,7 +36,7 @@ def db_execution(brand_name, subscription_id, resource_group_name, user_email, P
         main_cursor = main_db_conn.cursor(dictionary=True)
 
         ### Step 1: Fetch brand_id from main_db
-        brand_name = "Chase"
+        #brand_name = "Chase"
         main_cursor.execute("SELECT id FROM brand WHERE name = %s", (brand_name,))
         brand = main_cursor.fetchone()
         main_db_conn.commit()
