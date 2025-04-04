6d727fe2f2158ad2bfe0c94033cca391540abdae
z-aditya-pradhan <163983105+z-aditya-pradhan@users.noreply.github.com>
2025-04-04 11:27:46 +0530

    Update GenAI_automation.py

--- PyCode/GenAI_automation.py
+++ PyCode/GenAI_automation.py
@@ -178,7 +178,7 @@ def main():
         resources = create_openai_resources(rg_name, brand, SUBSCRIPTION_ID)
         all_resources.extend(resources)
 
-    brand = BRANDS[0].replace("-Pay-As-You-Go", "").replace("-", "").replace(" ", "")
+    brand = BRANDS[0].replace("-Pay-As-You-Go", "")
     output_json = {
         "subscription_id": SUBSCRIPTION_ID,
         "resources": all_resources,
