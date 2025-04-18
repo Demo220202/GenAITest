180b9568955caae0c811bb434edc5c1d2e7fea19
z-aditya-pradhan <163983105+z-aditya-pradhan@users.noreply.github.com>
2025-04-18 19:36:00 +0530

    Update pipeline.groovy

--- PyCode/pipeline.groovy
+++ PyCode/pipeline.groovy
@@ -15,7 +15,7 @@ pipeline{
                         . venv/bin/activate
                         pip install --upgrade pip
                         pip install -r requirements.txt
-                        python3 GenAI_automation.py --subscription_id $subscription_id --region $rg_region --deployment_model_name $dep_model_name --deployment_model_version $dep_model_version --brands $brand_names --deployment_type $dep_type
+                        python3 GenAI_automation.py --subscription_id $subscription_id --region $rg_region --deployment_model_name $dep_model_name --deployment_model_version $dep_model_version --brands "$brand_names" --deployment_type $dep_type
                     '''
                 }
             }
