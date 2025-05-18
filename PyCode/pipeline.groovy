a0ecc7f92c241522a1410f6c67652a8c60b6da3f
Mac <mac@Macs-MacBook-Pro-57.local>
2025-05-19 01:34:49 +0530

    Pipeline reverted to original one

--- PyCode/pipeline.groovy
+++ PyCode/pipeline.groovy
@@ -7,45 +7,45 @@ pipeline{
     }
     stages{
         
-//         stage ('Resource Group and OpenAI Resource Creation'){
-//             steps{
-//                 dir("PyCode"){
-//                     sh '''
-//                         python3.10 -m venv venv
-//                         . venv/bin/activate
-//                         pip install --upgrade pip
-//                         pip install -r requirements.txt
-//                         python3 GenAI_automation.py --subscription_id $subscription_id --region $rg_region --deployment_model_name $dep_model_name --deployment_model_version $dep_model_version --brands "$brand_names" --deployment_type $dep_type --env_m $env_main --email $user_email
-//                     '''
-//                 }
-//             }
-//         }
-//
-//         stage ('Deployment Resource Creation and Enabling Dynamic Quota'){
-//             steps{
-//                 dir("PyCode"){
-//                     sh '''
-//                         . venv/bin/activate
-//                         python3 GenAI_automation_P2.py
-//                     '''
-//                 }
-//             }
-//         }
-//
-//
-//         stage ('DB Insertions'){
-//
-//             steps{
-//                 dir("PyCode"){
-//                     sh '''
-//                         . venv/bin/activate
-//                         python3 GenAI_add_voices.py --env_m $env_main
-//                         python3 AzureGenAIResourceDBInsertions.py --env_m $env_main  --user_email $user_email
-//                         python3 ProdCoachDBInsertions.py --env $env_pa  --email $user_email --env_m $env_main
-//                     '''
-//                 }
-//             }
-//         }
+        stage ('Resource Group and OpenAI Resource Creation'){
+            steps{
+                dir("PyCode"){
+                    sh '''
+                        python3.10 -m venv venv
+                        . venv/bin/activate
+                        pip install --upgrade pip
+                        pip install -r requirements.txt
+                        python3 GenAI_automation.py --subscription_id $subscription_id --region $rg_region --deployment_model_name $dep_model_name --deployment_model_version $dep_model_version --brands "$brand_names" --deployment_type $dep_type --env_m $env_main --email $user_email
+                    '''
+                }
+            }
+        }
+
+        stage ('Deployment Resource Creation and Enabling Dynamic Quota'){
+            steps{
+                dir("PyCode"){
+                    sh '''
+                        . venv/bin/activate
+                        python3 GenAI_automation_P2.py
+                    '''
+                }
+            }
+        }
+
+
+        stage ('DB Insertions'){
+
+            steps{
+                dir("PyCode"){
+                    sh '''
+                        . venv/bin/activate
+                        python3 GenAI_add_voices.py --env_m $env_main
+                        python3 AzureGenAIResourceDBInsertions.py --env_m $env_main  --user_email $user_email
+                        python3 ProdCoachDBInsertions.py --env $env_pa  --email $user_email --env_m $env_main
+                    '''
+                }
+            }
+        }
 
         stage ('Checking DB Conection!'){
             steps{
@@ -58,16 +58,16 @@ pipeline{
             }
         }
 
-//         stage ('Creation of Action Group and Alerts'){
-//             steps{
-//                 dir("PyCode"){
-//                     sh '''
-//                         . venv/bin/activate
-//                         python3 ActionGroupNAlerts.py
-//                     '''
-//                 }
-//             }
-//         }
+        stage ('Creation of Action Group and Alerts'){
+            steps{
+                dir("PyCode"){
+                    sh '''
+                        . venv/bin/activate
+                        python3 ActionGroupNAlerts.py
+                    '''
+                }
+            }
+        }
         
     }
     post {
