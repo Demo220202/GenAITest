fb4d8a7f64b4f4531b5b8c4c5b1b5c15f34ecc19
Mac <mac@ip-172-18-255-228.us-west-1.compute.internal>
2025-06-26 13:38:43 +0530

    Temp removal of Add Voices

--- PyCode/pipeline.groovy
+++ PyCode/pipeline.groovy
@@ -39,7 +39,7 @@ pipeline{
                 dir("PyCode"){
                     sh '''
                         . venvgenai/bin/activate
-                        python3 GenAI_add_voices.py --env_m $env_main
+                        # python3 GenAI_add_voices.py --env_m $env_main
                         python3 AzureGenAIResourceDBInsertions.py --env_m $env_main  --user_email $user_email
                         python3 GenAIPredictionWrapperDB.py --user_email $user_email --env_m $env_main
                         python3 ProdCoachDBInsertions.py --env $env_pa  --email $user_email --env_m $env_main
